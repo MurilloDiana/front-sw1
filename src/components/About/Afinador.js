a57afe868250e4d3d566d1147757b365269b87b2
bikejojo <jorgecsri@gmail.com>
2023-11-07 18:25:48 -0400

    commit de afinador

---
+++ src/components/About/Afinador.js
@@ -0,0 +1,48 @@
+import React,{ useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import Particle from "../Particle";
+
+function Afinador(){
+    useEffect(() => {
+        // Manejar el mensaje enviado desde la ventana embebida
+        const handleMessage = (event) => {
+          // Aquí puedes procesar el mensaje recibido
+          console.log("Mensaje recibido desde la ventana embebida:", event.data);
+        };
+    
+        // Agregar un event listener para escuchar mensajes
+        window.addEventListener("message", handleMessage);
+    
+        // Limpiar el event listener cuando el componente se desmonta
+        return () => {
+          window.removeEventListener("message", handleMessage);
+        };
+      }, []);
+    return (
+    <Container fluid className="about-section">
+        <Particle />
+            <Container>
+                <Row style={{ justifyContent: "center", padding: "10px" }}>
+                    <Col
+                            md={7}
+                            style={{
+                            justifyContent: "center",
+                            paddingTop: "30px",
+                            paddingBottom: "50px",
+                            }}
+                    >
+                    <h2>Afinador Guitarras</h2>
+                    <iframe
+                        src="/tuner-master/app/index.html"
+                        title="Vista Embebida"
+                        width="600"
+                        height="400"
+                    ></iframe>
+                    </Col>
+                </Row>
+            </Container>
+    </Container>
+    );
+}
+
+export default Afinador;
